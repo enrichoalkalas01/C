bb74847ecfe6733e34e95d3facd1f66b95948cac
enrichoalkalas01 <enrichoalkalas01@gmail.com>
2018-03-19 11:33:39 +0700

    Create Penjumlahan,Pengurangan,Pembagian,Perkalian.cpp

---
+++ Penjumlahan,Pengurangan,Pembagian,Perkalian.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int x,y,hk,hkl,hb,hj ;
+  
+  cout << " Penjumlahan,Pengurangan,Perkalian,Pembagian " << endl ;
+  cout << " ------------------------------------------- " << endl ;
+  cout << " Masukan Angka Pertama : ;
+  cin >> x ;
+  cout << " Masukan Angka Kedua : ;
+  cin >> y ;
+  
+  hj = x+y ;
+  hk = x-y ;
+  hkl = x*y ;
+  hb = x/y ;
+  
+  cout << " ------------------------- " << endl ;
+  cout << " Hasilnya " << endl ;
+  cout << " Penjumlahan : " << hj << endl ;
+  cout << " Pengurangan : " << hk << endl ;
+  cout << " Perkalian   : " << hkl << endl ; 
+  cout << " Pembagian   : " << hb << endk ;
+  cout << " ------------------------- " << endl ;  
+}
